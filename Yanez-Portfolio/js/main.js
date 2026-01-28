eafe0cbb549af2fdb7a0b7908f85c441c7f7c918
NahumYanez <136136917+nahum-yanez@users.noreply.github.com>
2026-01-28 11:52:06 -0800

    Add files via upload

--- Yanez-Portfolio/js/main.js
+++ Yanez-Portfolio/js/main.js
@@ -4,3 +4,9 @@ const mainNav = document.getElementById("mainNav");
 navToggle.addEventListener("click", () => {
   mainNav.classList.toggle("show");
 });
+
+document.querySelectorAll("#mainNav a").forEach(link => {
+  link.addEventListener("click", () => {
+    mainNav.classList.remove("show");
+  });
+});
